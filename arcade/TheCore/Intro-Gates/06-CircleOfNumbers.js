e7cce27f812473d8258969c721eeb4f89e3ea89e
Waqas Malik <waqasmalik10@hotmail.com>
2024-08-14 21:24:59 +0500

    12th problem done in TheCore

---
+++ arcade/TheCore/Intro-Gates/06-CircleOfNumbers.js
@@ -0,0 +1,44 @@
+/************
+ * Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+
+For n = 10 and firstNumber = 2, the output should be
+solution(n, firstNumber) = 7.
+
+
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+A positive even integer.
+
+Guaranteed constraints:
+4 ≤ n ≤ 20.
+
+[input] integer firstNumber
+
+Guaranteed constraints:
+0 ≤ firstNumber ≤ n - 1.
+
+[output] integer
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+const solution = (n, firstNumber) => {
+    return (firstNumber+Math.floor(n/2)) % n;
+}
